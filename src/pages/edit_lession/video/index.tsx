778efd1ba7915ac15fbc6266ab1426e0c92d5cda
ndhung <nguyenduyhung04092004@gmail.com>
2024-12-07 02:24:26 +0700

    update query

--- src/pages/edit_lession/video/index.tsx
+++ src/pages/edit_lession/video/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Group, Stack, Text } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
+
+import classes from "./styles.module.css";
+import textClasses from "@/styles/text.module.css";
 
 
 
 const Video: React.FC = () => {
     return (
-        <>Video</>
+        <Stack w={"100%"} p={16}>
+            <Group
+                className={classes.add_chapter}
+            >
+                <IconPlus size={30} />
+                <Text className={textClasses.text_size_md}>Khởi tạo video</Text>
+            </Group>
+        </Stack>
     )
 }
 
